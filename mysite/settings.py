ece9f835af405306f0b03baa1589a93b8bbcc15b
Emmanouil Koriotakis (Trasys) <emmanouil.korniotakis@trasys.gr>
2023-10-07 01:25:40 +0300

    Add DNS drdakanalis and www.drdakanalis to heroku-app

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost', 'dj-drdaka-adf623812670.herokuapp.com', 'drdakanalis.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'dj-drdaka-adf623812670.herokuapp.com', 
+                 'drdakanalis.com',
+                 'www.drdakanalis.com'
+                ]
 
 
 # Application definition
